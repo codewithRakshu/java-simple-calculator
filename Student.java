da82ddd76d0ec34835294c7bfdd7391b2d32f620
Rakshitha N <110377451+codewithRakshu@users.noreply.github.com>
2025-05-02 12:10:53 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,25 @@
+public class Student {
+    private String name;
+    private int age;
+    private String id;
+
+    public Student(String name, int age, String id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public String getId() { return id; }
+
+    public void setName(String name) { this.name = name; }
+    public void setAge(int age) { this.age = age; }
+    public void setId(String id) { this.id = id; }
+
+    @Override
+    public String toString() {
+        return name + " | " + age + " | " + id;
+    }
+}
+
